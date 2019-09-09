c01321ccb1fb4c73568c5e7f30123bfee6fa6d9b
Chris Zehner <cbzehner@gmail.com>
2019-09-08 23:16:11 -0700

    chore: clean up unused parts of webpack config
    Signed-off-by: Chris Zehner <cbzehner@gmail.com>

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,17 +3,13 @@ const path = require("path")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-// const WriteFileWebpackPlugin = require("write-file-webpack-plugin")
 
 const rootPath = path.join(__dirname, "./..")
-// const fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"]
 
 const options = {
-  mode: "development",
   entry: {
     popup: `${rootPath}/src/js/popup.js`,
     content: `${rootPath}/src/js/content.js`,
-    // background: `${rootPath}/src/js/background.js"`,
   },
   output: {
     path: `${rootPath}/build`,
@@ -25,20 +21,14 @@ const options = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      // {
-      // test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
-      // loader: "file-loader?name=[name].[ext]",
-      // },
       {
         test: /\.html$/,
         use: ["html-loader"],
-        // use: ['file-loader?name=[name].[ext]', 'extract-loader', 'html-loader'],
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(), // Clean `build/` between webpack runs
-    // new WriteFileWebpackPlugin({ test: /\.css$/ }),    // ???
     new HtmlWebpackPlugin({   // Load HTML files
       template: `${rootPath}/src/html/popup.html`,
       filename: "popup.html",
@@ -48,8 +38,8 @@ const options = {
       [
         { 
           from: 'manifest.json', 
+          // Use the package.json to populate the manifest.json
           transform: function (content, _path) {
-            // Use the package.json to populate the manifest.json
             return Buffer.from(JSON.stringify({
               description: process.env.npm_package_description,
               version: process.env.npm_package_version,
